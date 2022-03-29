d5393685d81f377e2bc05df65a2c5bf79284ada5
Nguyen Long <nguyenlong232253@gmail.com>
2022-03-29 17:02:32 +0700

    them bai Comparable

--- src/DoiTuongHinhHoc/CircleTest.java
+++ src/DoiTuongHinhHoc/CircleTest.java
@@ -1,5 +1,9 @@
 package DoiTuongHinhHoc;
 
+import comparable.ComparableCircle;
+
+import java.util.Arrays;
+
 public class CircleTest {
     public static void main(String[] args) {
         Circle circle = new Circle();
@@ -10,6 +14,23 @@ public class CircleTest {
 
         circle = new Circle("indigo", false, 3.5);
         System.out.println(circle);
+
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle("indigo", false, 3.5);
+
+        System.out.println("Pre-sorted: ");
+        for (ComparableCircle circle1 : circles ){
+            System.out.println(circle1);
+        }
+
+        Arrays.sort(circles);
+
+        System.out.println("Affter-sorted");
+        for (ComparableCircle circle1 : circles){
+            System.out.println(circle1);
+        }
 
     }
 }
